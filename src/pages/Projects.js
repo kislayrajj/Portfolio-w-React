1d91a1f4c32d7fbfc1edfa8dee13947bd11db6df
Kislay Raj <kislayraj751@gmail.com>
2024-03-03 22:09:26 +0530

    trying to reflct changes

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -3,10 +3,9 @@ import { useState } from "react";
 import { useEffect } from "react";
 import { Link } from "react-router-dom";
 import PortfThumb from "../assests/portfolioWeb.png";
-import MAcard from '../assests/MAcard.png'
-import RockPaperScissor from "../assests/RockPaperScissor.png"
-import ReactCardThumb from '../assests/ReactCard-Thumb.png'
-
+import MAcard from "../assests/MAcard.png";
+import RockPaperScissor from "../assests/RockPaperScissor.png";
+import ReactCardThumb from "../assests/ReactCard-Thumb.png";
 
 const Projects = () => {
   const [dateTime, setDateTime] = useState("");
@@ -20,55 +19,79 @@ const Projects = () => {
     };
     updateDateTime();
     const intervalId = setInterval(updateDateTime, 1000);
+
     return () => clearInterval(intervalId);
   });
 
   return (
-    
     <div className=" w-full   flex flex-col gap-6 items-center  justify-around md:flex-row md:gap-1 md:items-start m-2 pt-16 text-xs ">
       <div
         className="box-card clock border-2 rounded-xl h-72 w-64 p-8 text-lg"
         dangerouslySetInnerHTML={{ __html: dateTime }}
       ></div>
-      <Link to="https://kislayrajj.github.io/DynamicCard-html-css/" target="_blank">
+      <Link
+        to="https://kislayrajj.github.io/DynamicCard-html-css/"
+        target="_blank"
+      >
         <div className="box-card border-2 rounded-xl h-72 w-64 text-red-700">
-         <img src={MAcard} alt="" className="h-44 flex pl-16  rounded-lg"/>
-         <div className=" p-2 pt-4 text-gray-300 ">•Dynamic Businnes Card</div>
+          <img src={MAcard} alt="" className="h-44 flex pl-16  rounded-lg" />
+          <div className=" p-2 pt-4 text-gray-300 ">•Dynamic Businnes Card</div>
           <div className="pl-2 pt-0 text-gray-300">
-          •This is card design dedicated to Marcus Aurelius <br />
+            •This is card design dedicated to Marcus Aurelius <br />
           </div>
           <div className="pl-2 text-gray-300">•Language used - HTML & CSS</div>
         </div>
       </Link>
-      <Link  to="https://kislayrajj.github.io/DevMen-Portfolio/" target="_blank"><div className="box-card border-2 rounded-xl h-80 w-72 ">
-      <img src={PortfThumb} alt="" className=" object-fill rounded-t-lg " />
-          <div className=" p-2 pt-4  text-gray-300">•Personalized Portfolio</div>
+      <Link to="https://kislayrajj.github.io/DevMen-Portfolio/" target="_blank">
+        <div className="box-card border-2 rounded-xl h-80 w-72 ">
+          <img src={PortfThumb} alt="" className=" object-fill rounded-t-lg " />
+          <div className=" p-2 pt-4  text-gray-300">
+            •Personalized Portfolio
+          </div>
           <div className="p-2 pt-0 text-gray-300">
-          •This was the first proper website i built. Take a look by visiting
+            •This was the first proper website i built. Take a look by visiting
             the website. <br />
             Click to visit.
           </div>
-          <div className="p-2 text-gray-300 ">•Language used - HTML, CSS & JS</div></div></Link>
-      <Link to="https://kislayrajj.github.io/Rock-Paper-Scissor/" target="_blank"><div className="box-card border-2 rounded-xl h-72 w-64">
-        <img src={RockPaperScissor} alt="" className="rounded-t-lg" />
-        <div className=" p-2 pt-4  text-gray-300">•Rock-Paper-Scissor Game</div>
+          <div className="p-2 text-gray-300 ">
+            •Language used - HTML, CSS & JS
+          </div>
+        </div>
+      </Link>
+      <Link
+        to="https://kislayrajj.github.io/Rock-Paper-Scissor/"
+        target="_blank"
+      >
+        <div className="box-card border-2 rounded-xl h-72 w-64">
+          <img src={RockPaperScissor} alt="" className="rounded-t-lg" />
+          <div className=" p-2 pt-4  text-gray-300">
+            •Rock-Paper-Scissor Game
+          </div>
           <div className="p-2 pt-0 text-gray-300">
-          •I built this simple version of the game while learnig JavaScript. <br />
+            •I built this simple version of the game while learnig JavaScript.{" "}
+            <br />
             Click to give it a try.
           </div>
-          <div className="pl-2 text-gray-300">•Language used - HTML, CSS & JS</div>
-        </div></Link>
-      <Link to="/ReactCard"><div className="box-card border-2 rounded-xl h-72 w-64">
-        <img src={ReactCardThumb} alt="" className="" />
-        <div className=" p-2 pt-4 pb-0 text-gray-300">•Business Card</div>
+          <div className="pl-2 text-gray-300">
+            •Language used - HTML, CSS & JS
+          </div>
+        </div>
+      </Link>
+      <Link to="/ReactCard">
+        <div className="box-card border-2 rounded-xl h-72 w-64">
+          <img src={ReactCardThumb} alt="" className="" />
+          <div className=" p-2 pt-4 pb-0 text-gray-300">•Business Card</div>
           <div className="p-2 pt-0 pb-0 text-gray-300">
-          •Custom business card design using React for a personalized touch. <br />
+            •Custom business card design using React for a personalized touch.{" "}
+            <br />
             Click to view.
           </div>
-          <div className="pl-2  text-gray-300">•Language used - React & Tailwind CSS</div>
-        </div></Link>
+          <div className="pl-2  text-gray-300">
+            •Language used - React & Tailwind CSS
+          </div>
+        </div>
+      </Link>
     </div>
-   
   );
 };
 
