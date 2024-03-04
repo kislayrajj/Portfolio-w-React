8ab91ceba2d27dbaa02387362a5a09466a59271f
Kislay Raj <kislayraj751@gmail.com>
2024-03-04 20:20:27 +0530

    used flex wrap

--- src/components/MainContent.js
+++ src/components/MainContent.js
@@ -7,7 +7,7 @@ const MainContent = () => {
 
 
   return (
-    <div className=' flex justify-around m-5 w-full'>
+    <div className=' flex flex-wrap justify-around m-5 w-full'>
       <div className="box-card border-2 rounded-xl h-64 w-64">1</div>
       <div className="box-card border-2 rounded-xl h-64 w-64">2</div>
       <div className="box-card border-2 rounded-xl h-72 w-72">3</div>
