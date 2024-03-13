94cf1282c3aab05f7e44bda4161ef3f41a93de8f
Kislay Raj <kislayraj751@gmail.com>
2024-03-13 17:19:14 +0530

    edited about section

--- src/components/About.js
+++ src/components/About.js
@@ -1,12 +1,36 @@
 import React from 'react'
 
-const About = () => {
+const About = ({ thumbVideo, service, ListOne, ListTwo, ListThree, snumber}) => {
   return (
-    <div>
-      <div className=' about h-screen w-full text-center '>
-        hi
+      <div
+       className='webdev  border-2 flex justify-between flex-grow p-2  sm:w-1/3 md:max-w-[500px]  h-auto md:min-h-72 relative  text-white/90  rounded-md overflow-hidden'>
+<div>
+<div className='  font-bold text-md md:text-xl tracking-wider pl-2'>{service}
+          </div>
+         
+          <ul className='list-disc  pt-2 md:pt-5 pl-2 md:pl-10 text-gray-100/85 text-xs md:text-base font-semibold tracking-wider'> 
+            <li >{ListOne}</li>
+            <li>{ListTwo} </li>
+            <li>{ListThree}</li>
+          </ul>
+          <div className='absolute bottom-[-40px] z-[] text-9xl text-green-500/40 '>{snumber}</div>
       </div>
-    </div>
+      <div className=' object-fillh-44 w-44 overflow-hidden  md:min-w-44 mt-10'>
+           {/* <img src={img} alt="" className='h-52 rounded-md'/> */}
+           
+           <video
+            autoPlay
+            muted
+            loop
+            className=" p-2 rounded-3xl  absolute md:static top-0 right-0 opacity-10 md:opacity-90 "
+          >
+            <source src={thumbVideo} type="video/mp4" />
+          </video>
+          </div>
+</div>
+   
+    
+    
   )
 }
 
--- src/components/MainContent.js
+++ src/components/MainContent.js
@@ -7,7 +7,15 @@ import cssLogo from "../assests/csspng.png";
 import jsLogo from "../assests/jspng.png";
 import reactLogo from "../assests/reactpng.png";
 import tailwindLogo from "../assests/tailwindpng.png";
-import About from './About'
+import About from "./About";
+import websiteDev_img from "../assests/websitedev.png"
+import thumbVideobg from "../assests/website-development.mp4"
+import thumbVideobg2 from '../assests/thumbnail-of-web-application-development.mp4'
+import thumbVideobg3 from '../assests/ux-designer.mp4'
+import thumbVideobg4 from '../assests/texhnical-experties.mp4'
+import thumbVideobg5 from '../assests/digital-marketing.mp4'
+import thumbVideobg6 from '../assests/thumbnail-of-maintenance.mp4'
+
 const MainContent = () => {
   const [isHovered, setIsHovered] = useState(false);
 
@@ -15,14 +23,14 @@ const MainContent = () => {
     <div>
       <div>
         <div className="main-component flex  justify-around items-center  w-full h-screen">
-          <video
+          {/* <video
             autoPlay
             muted
             loop
             className="absolute  inset-0 object-cover ml- w-full h-full z-[-1]"
           >
             <source src={bgVideo} type="video/mp4" />
-          </video>
+          </video> */}
           <div className="sloga mt-32 md:mt-0">
             <h1
               className=" text-7xl m-2 md:text-8xl font-bold  "
@@ -38,9 +46,72 @@ const MainContent = () => {
           </div>
         </div>
       </div>
-<div>
-  <About />
-</div>
+      <div>
+        <div className=" about h-auto w-full text-gray-700 p-5  ">
+          <h1 className=" bg-gradient-to-r from-red-500 via-blue-500 to-black text-transparent bg-clip-text font-extrabold text-3xl ">What we do :</h1>
+          <p className=" md:w-1/2 3/4 m-2  text-xs md:text-base text-gray-700">
+            At DevStudio, we specialize in delivering top-notch web development
+            solutions tailored to meet your unique needs. With our expert team
+            of developers, designers, and strategists, we offer a comprehensive
+            range of services to help you succeed online. Here's what we can do
+            for you:
+          </p>
+          <div className="Boxes m-10 flex flex-wrap justify-between gap-2 ">
+          <About 
+          thumbVideo={thumbVideobg}
+          snumber="01"
+          service="Website Developments"
+          img={websiteDev_img}
+          ListOne="Custom Website Design"
+          ListTwo="Responsive Development"
+          ListThree="E-commerce Solutions"
+        
+          />
+          <About 
+          snumber="02"
+          thumbVideo={thumbVideobg2}
+              service="Web Application Development"
+              ListOne="Bespoke Web Applications: "
+              ListTwo="API Integration"
+              ListThree="Content Management Systems (CMS)"
+           />
+          <About 
+          snumber="03"
+          thumbVideo={thumbVideobg3}
+              service="UI/UX Design"
+              ListOne="User-Centric Design"
+              ListTwo="Wireframing and Prototyping"
+              ListThree="Usability Testing"
+         />
+          <About
+          snumber="04"
+          thumbVideo={thumbVideobg4}
+              service="Technology Expertise"
+              ListOne="Frontend Development"
+              ListTwo="Backend Development:"
+              ListThree="Database Management & Cloud Services"
+ />
+          <About 
+          snumber="05"
+          thumbVideo={thumbVideobg5}
+              service="Digital Strategy & Marketing"
+              ListOne="Consultation and Planning"
+              ListTwo="Digital Marketing"
+              ListThree="Search Engine Optimization (SEO)"
+         />
+          <About 
+
+          snumber="06"
+          thumbVideo={thumbVideobg6}
+              service="Support and Maintenance"
+              ListOne="Ongoing Support:"
+              ListTwo="Performance Monitoring"
+        
+           />
+        </div>
+        </div>
+     
+      </div>
       <div className=" skills flex  flex-wrap justify-around pt-12 h-screen">
         <Skills
           image={htmlLogo}
