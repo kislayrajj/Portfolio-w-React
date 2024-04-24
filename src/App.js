a1af2a616965042f6a9416ef33b3f22f7591d04c
Kislay Raj <kislayraj751@gmail.com>
2024-04-24 22:51:48 +0530

    first commit on suspese and lazy

--- src/App.js
+++ src/App.js
@@ -2,9 +2,14 @@ import "./App.css";
 import { Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import MainContent from "./components/MainContent.js";
-import Projects from "./pages/Projects.js";
+// import Projects from "./pages/Projects.js";
 import About from "./components/About.js";
 import Services from "./components/Services.js";
+import { lazy,Suspense } from 'react';
+import Loading from "./components/Loading.js";
+
+
+const Projects = lazy(() => import('./pages/Projects.js'));
 function App() {
   return (
     <div className="Main scroll-smooth">
@@ -12,12 +17,16 @@ function App() {
         <Navbar />
       </div>
 
+<Suspense fallback={< Loading/>}>
       <Routes>
         <Route path="/" element={<MainContent />}></Route>
         <Route path="/Projects" element={<Projects />}></Route>
+  
         {/* <Route path="/About" element={<About />}></Route>
         <Route path="/Services" element={<Services />}></Route> */}
       </Routes>
+      </Suspense>
+      < Loading/>
     </div>
   );
 }
---
+++ src/components/Loading.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Loading = () => {
+  return (
+    <div className='h-screen center bg-gray-800'>
+     
+     <div className='text-4xl theme-font text-green-400'>Loading..</div>
+      
+    </div>
+  )
+}
+
+export default Loading
