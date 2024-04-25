14139f5e8d264d07b59bc21cfaf6ecbf3610b643
Kislay Raj <kislayraj751@gmail.com>
2024-04-25 13:15:11 +0530

    added few loader animations

--- src/App.js
+++ src/App.js
@@ -1,33 +1,60 @@
 import "./App.css";
 import { Routes, Route } from "react-router-dom";
+import { useState } from "react";
+import { useEffect } from "react";
 import Navbar from "./components/Navbar";
 import MainContent from "./components/MainContent.js";
 // import Projects from "./pages/Projects.js";
 import About from "./components/About.js";
 import Services from "./components/Services.js";
-import { lazy,Suspense } from 'react';
+import { lazy, Suspense } from "react";
 import Loading from "./components/Loading.js";
+import LoadingAnimation from "./components/Loaders/Loader2.json";
+import Lottie from "lottie-react";
 
+// import LoadingAnimation2 from "./components/Loaders/Loader1.json";
 
-const Projects = lazy(() => import('./pages/Projects.js'));
+const Projects = lazy(() => import("./pages/Projects.js"));
 function App() {
+
+  // increase loading time
+  const [isLoading, setIsLoading] = useState(true);
+  useEffect(() => {
+    setTimeout(() => {
+      setIsLoading(false);
+    }, 4000); 
+  }, []);
+
+
+
   return (
+    <>
+    {isLoading ? (<div className="h-screen center">
+       
+       <Lottie animationData={LoadingAnimation} className="h-44 md:h-80" />
+     </div> ) : (
     <div className="Main scroll-smooth">
       <div className="relative">
         <Navbar />
       </div>
 
-<Suspense fallback={< Loading/>}>
-      <Routes>
-        <Route path="/" element={<MainContent />}></Route>
-        <Route path="/Projects" element={<Projects />}></Route>
-  
-        {/* <Route path="/About" element={<About />}></Route>
+      <Suspense fallback={<Loading />}>
+        <Routes>
+          <Route path="/" element={<MainContent />}></Route>
+          <Route path="/Projects" element={<Projects />}></Route>
+
+          {/* <Route path="/About" element={<About />}></Route>
         <Route path="/Services" element={<Services />}></Route> */}
-      </Routes>
+        </Routes>
       </Suspense>
-      < Loading/>
+      <Loading />
+      {/* <div className="h-screen center">
+       
+       <Lottie animationData={LoadingAnimation2} className="h-44 md:h-80" />
+     </div> */}
     </div>
+    )}
+    </>
   );
 }
 
--- src/components/Loading.js
+++ src/components/Loading.js
@@ -2,9 +2,9 @@ import React from 'react'
 
 const Loading = () => {
   return (
-    <div className='h-screen center bg-gray-800'>
+    <div className='h-screen center background'>
      
-     <div className='text-4xl theme-font text-green-400'>Loading..</div>
+     <div className='text-xl md:text-4xl theme-font text-green-400'>Loading..</div>
       
     </div>
   )
