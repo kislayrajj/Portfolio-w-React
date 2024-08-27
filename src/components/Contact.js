4b8956a743eaffa83cace81baf210516a0acf480
kislayrajj <kislayraj751@gmail.com>
2024-08-27 19:21:10 +0530

    working contact form

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,18 +1,20 @@
 import React, { useEffect, useRef, useState } from "react";
 import { motion } from "framer-motion";
 import { useSelector } from "react-redux";
-import axios from "axios";
 const Contact = () => {
   const contactRef = useRef(null);
   const [isVisible, setIsVisible] = useState(false);
   const [isFormDark, setIsFormDark] = useState(true);
+  const [isErrorInFormSubmission, setIsErrorInFormSubmission] = useState(false);
+  const [isSubmitting, setIsSubmitting] = useState(false);
+  const [submissionSuccess, setSubmissionSuccess] = useState(false);
+
   const [formData, setFormData] = useState({
     name: "",
     email: "",
-    subject: "",
+    company: "",
     message: "",
   });
-  const [isErrorInFormSubmission, setIsErrorInFormSubmission] = useState(false);
 
   const themeColor = useSelector((state) => state.theme.themeColor);
 
@@ -27,12 +29,12 @@ const Contact = () => {
 
     const currentRef = contactRef.current;
     if (currentRef) {
-      observer.observe(currentRef);
+      observer?.observe(currentRef);
     }
 
     return () => {
       if (currentRef) {
-        observer.unobserve(currentRef);
+        observer?.unobserve(currentRef);
       }
     };
   }, []);
@@ -53,22 +55,36 @@ const Contact = () => {
 
   const handleSubmit = async (e) => {
     e.preventDefault();
+    setIsSubmitting(true);
+    setSubmissionSuccess(false);
+
     try {
-      const response = await axios.post(
-        "http://localhost:8000/submit-form",
-        formData
-      );
-      if (response.status === 200) {
-        console.log("form submitted successfully : ", response.data);
+      const formDataWithAccessKey = {
+        ...formData,
+        access_key: "cc7bab87-183d-4401-af8f-3023601e7c83",
+      };
+      const response = await fetch("https://api.web3forms.com/submit", {
+        method: "POST",
+        headers: {
+          "content-type": "application/json",
+          Accept: "application/json",
+        },
+        body: JSON.stringify(formDataWithAccessKey),
+      });
+      const result = await response.json();
+
+      if (result.success) {
+        console.log("form submitted successfully : ", result.data);
         setFormData({
           name: "",
           email: "",
-          subject: "",
+          company: "",
           message: "",
         });
         setIsErrorInFormSubmission(false);
+        setSubmissionSuccess(true);
       } else {
-        console.error("Error occurred  : ", response.statusText);
+        console.error("Error occurred  : ", result.message);
         setIsErrorInFormSubmission(true);
 
         setTimeout(() => {
@@ -76,11 +92,17 @@ const Contact = () => {
         }, 5000);
       }
     } catch (error) {
-      console.error("Error submitting form : ", error);
+      console.error("Error submitting form (catchError) : ", error);
       setIsErrorInFormSubmission(true);
 
       setTimeout(() => {
         setIsErrorInFormSubmission(false);
+      }, 5000);
+    } finally {
+      setIsSubmitting(false); // Stop loader
+      setTimeout(() => {
+        setIsErrorInFormSubmission(false);
+        setSubmissionSuccess(false);
       }, 5000);
     }
   };
@@ -172,15 +194,16 @@ const Contact = () => {
         <div className="center">
           <div className="relative mt-10 lg:mt-0 ">
             <motion.span
-             whileHover={{ y: -10 }} // Move the text 10px up on hover
-             transition={{ duration: .2 }} //
+              whileHover={{ y: -10 }} // Move the text 10px up on hover
+              transition={{ duration: 0.2 }} //
               className={` lg:absolute               transition-all duration-500 ease-in-out lg:text-5xl ${
-                themeColor === "dark" ? "text-white lg:hover:bg-gradient-to-r lg:hover:from-slate-300 lg:hover:to-slate-500 lg:hover:bg-clip-text lg:hover:text-transparent" : "text-black lg:hover:bg-gradient-to-r lg:hover:from-rose-400 lg:hover:to-red-500 lg:hover:bg-clip-text lg:hover:text-transparent"
+                themeColor === "dark"
+                  ? "text-white lg:hover:bg-gradient-to-r lg:hover:from-slate-300 lg:hover:to-slate-500 lg:hover:bg-clip-text lg:hover:text-transparent"
+                  : "text-black lg:hover:bg-gradient-to-r lg:hover:from-rose-400 lg:hover:to-red-500 lg:hover:bg-clip-text lg:hover:text-transparent"
               }`}>
               Send me a text
             </motion.span>{" "}
-            <i className="fa-solid fa-paper-plane lg:absolute lg:text-5xl lg:right-12 lg:hover:scale-105"
-            ></i>
+            <i className="fa-solid fa-paper-plane lg:absolute lg:text-5xl lg:right-12 lg:hover:scale-105"></i>
             <div
               className={`relative mt-2 lg:mt-9  w-[300px]  sm:w-[400px] lg:w-[500px] xl:w-[600px]  mx-auto p-6  rounded-lg shadow-md ${
                 themeColor === "dark" ? "shadow-form_dark" : "shadow-form_light"
@@ -199,6 +222,20 @@ const Contact = () => {
                 )}
               </div>
               <form className="space-y-4 relative" onSubmit={handleSubmit}>
+                {isSubmitting && (
+                  <div className="absolute  inset-0  size-full center">
+                    <div className="flex items-center justify-center bg-white/40  z-10 rounded-md w-1/2 h-[40%] ">
+                      <div className="formSubLoader"></div>
+                    </div>
+                  </div>
+                )}
+                {/* Success Message */}
+                {/* {submissionSuccess && ( )} */}
+
+                <div className="absolute -bottom-3 md:bottom-0  text-[10px] md:text-xs lg:text-base w-min md:w-auto mr-2 p-2 bg-green-500/60 text-white rounded">
+                  Form submitted successfully!
+                </div>
+
                 <motion.div
                   initial={{ opacity: 0, y: 150 }}
                   animate={isVisible ? { opacity: 1, y: 0 } : ""}
@@ -259,19 +296,19 @@ const Contact = () => {
                   animate={isVisible ? { opacity: 1, y: 0 } : ""}
                   transition={{ duration: 1, delay: 0.4 }}>
                   <label
-                    htmlFor="subject"
+                    htmlFor="company"
                     className={`block text-sm lg:text-base font-medium 
             ${isFormDark ? "text-white" : "text-black"} `}>
-                    Subject
+                    Company
                   </label>
                   <input
                     type="text"
-                    id="subject"
-                    name="subject"
-                    value={formData.subject}
+                    id="company"
+                    name="company"
+                    value={formData.company}
                     onChange={handleChange}
                     required
-                    placeholder="Enter the subject"
+                    placeholder="Enter Your company name"
                     className={`mt-1 py-2 px-2 block w-full rounded-md shadow-sm focus:outline-none
               focus:outline-green-500 sm:text-sm lg:text-base 
               ${
@@ -325,13 +362,13 @@ const Contact = () => {
                     animate={isVisible ? { opacity: 1, y: 0 } : ""}
                     transition={{ duration: 1, delay: 0.6 }}
                     type="submit"
-                    className={`inline-flex items-center px-4 py-2 border border-transparent text-sm font-medium rounded-md
+                    className={`inline-flex items-center px-4 py-2 border border-transparent text-sm font-medium rounded-md w-24 center justify-center
               ${
                 isFormDark
                   ? "shadow-[0px_0px_20px_2px_#e2e8f0] text-white"
                   : "shadow-[0px_0px_20px_2px_#f687b3] text-black"
               }`}>
-                    Submit
+                    {isSubmitting ? "Submitting..." : "Submit "}
                   </motion.button>
                 </div>
               </form>
--- src/components/logos/NameLogoGsap.js
+++ src/components/logos/NameLogoGsap.js
@@ -1,15 +1,15 @@
-import React, { useEffect, useRef } from 'react';
-import { gsap } from 'gsap';
-import SplitType from 'split-type';
-import { useSelector } from 'react-redux';
+import React, { useEffect, useRef } from "react";
+import { gsap } from "gsap";
+import SplitType from "split-type";
+import { useSelector } from "react-redux";
 
 const NameLogoGsap = () => {
   const textRef = useRef();
   const svgRef = useRef();
-  const themeColor = useSelector((state)=>state.theme.themeColor)
+  const themeColor = useSelector((state) => state.theme.themeColor);
 
   useEffect(() => {
-    const splitText = new SplitType(textRef.current, { types: 'chars' });
+    const splitText = new SplitType(textRef.current, { types: "chars" });
 
     gsap.fromTo(
       splitText.chars,
@@ -18,7 +18,7 @@ const NameLogoGsap = () => {
         opacity: 1,
         y: 0,
         duration: 1,
-        ease: 'power4.out',
+        ease: "power4.out",
         stagger: 0.05,
       }
     );
@@ -30,7 +30,7 @@ const NameLogoGsap = () => {
         opacity: 1,
         scale: 1,
         duration: 1.5,
-        ease: 'elastic.out(1, 0.3)',
+        ease: "elastic.out(1, 0.3)",
         delay: 1,
       }
     );
@@ -43,7 +43,9 @@ const NameLogoGsap = () => {
 
   return (
     <div className="relative flex items-center">
-      <h1 ref={textRef} className="lg:hidden xl:block text-lg  lg:text-xl xl:text-2xl 2xl:text-5xl grad font-bold leading-tight ">
+      <h1
+        ref={textRef}
+        className="lg:hidden xl:block text-lg  lg:text-xl xl:text-2xl 2xl:text-5xl grad font-bold leading-tight">
         Kislay Raj
       </h1>
       <svg
@@ -51,16 +53,26 @@ const NameLogoGsap = () => {
         className="absolute top-1/2 left-2 transform -translate-x-1/2 -translate-y-1/2 w-8 h-8 lg:w-16 lg:h-16 xl:w-24 xl:h-24 -z-10"
         viewBox="0 0 100 100"
         fill="none"
-        xmlns="http://www.w3.org/2000/svg"
-      >
-        {themeColor ==="dark" ?  <circle cx="50" cy="50" r="40" stroke="white" strokeWidth="5" /> : <circle cx="50" cy="50" r="40" stroke="deepPink" strokeWidth="5" /> }
-       {themeColor ==="dark" ?   <circle cx="50" cy="50" r="30" stroke="blue" strokeWidth="5" /> : <circle cx="50" cy="50" r="30" stroke="#06D001" strokeWidth="5" /> }
+        xmlns="http://www.w3.org/2000/svg">
+        {themeColor === "dark" ? (
+          <circle cx="50" cy="50" r="40" stroke="white" strokeWidth="5" />
+        ) : (
+          <circle cx="50" cy="50" r="40" stroke="deepPink" strokeWidth="5" />
+        )}
+        {themeColor === "dark" ? (
+          <circle cx="50" cy="50" r="30" stroke="blue" strokeWidth="5" />
+        ) : (
+          <circle cx="50" cy="50" r="30" stroke="#06D001" strokeWidth="5" />
+        )}
 
-       {themeColor ==="dark" ?    <circle cx="50" cy="50" r="20" stroke="#06D001" strokeWidth="5" /> : <circle cx="50" cy="50" r="20" stroke="blue" strokeWidth="5" /> }
+        {themeColor === "dark" ? (
+          <circle cx="50" cy="50" r="20" stroke="#06D001" strokeWidth="5" />
+        ) : (
+          <circle cx="50" cy="50" r="20" stroke="blue" strokeWidth="5" />
+        )}
         {/* <circle cx="50" cy="50" r="30" stroke="blue" strokeWidth="5" />
         <circle cx="50" cy="50" r="20" stroke="#06D001" strokeWidth="5" /> */}
-         {/* <circle cx="50" cy="50" r="10" stroke="violet" strokeWidth="5" /> */}
-
+        {/* <circle cx="50" cy="50" r="10" stroke="violet" strokeWidth="5" /> */}
       </svg>
     </div>
   );
